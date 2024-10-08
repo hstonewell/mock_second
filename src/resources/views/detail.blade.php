265952e7884a850d44895383c5cc9053d3f38d92
haruna ishii <eijen.6@gmail.com>
2024-10-08 18:18:40 +0900

    CSSの構成を修正

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -9,22 +9,22 @@
 @endsection
 
 @section('content')
-<div class="shopd-detail__wrapper">
-    <div class="shop-detail__block">
+<div class="shop-detail__wrapper">
+    <div class="shop-detail__block--left">
         <div class="shop-detail__title">
             <a href="/" class="shop-detail--back">&lt;</a>
             <h2 class="shop-name">{{ $shop->shop_name }}</h2>
         </div>
         <img src="{{ $shop->image }}" alt="{{ $shop->shop_name }}">
         <div class="shop-tags">
-            <p>&#035;{{ $shop->area->area_name }}</p>
-            <p>&#035;{{ $shop->genre->genre_name }}</p>
+            <a href="{{ route('search', ['area_id' => $shop->area->id])}}">&#035;{{ $shop->area->area_name }}</a>
+            <a href="{{ route('search', ['genre_id' => $shop->genre->id])}}">&#035;{{ $shop->genre->genre_name }}</a>
         </div>
         <div class="shop-detail__description">
             {{ $shop->detail }}
         </div>
     </div>
-    <div class="shop-detail__block">
+    <div class="shop-detail__block--right">
         <div class="booking__box">
             <h2 class="booking__title">予約</h2>
             <div class="booking__box--main">
