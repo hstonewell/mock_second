6d5b916641ca879f78c6cb243a1205b40d5af015
haruna ishii <eijen.6@gmail.com>
2024-10-08 18:20:18 +0900

    予約機能修正

--- src/app/Http/Controllers/ShopController.php
+++ src/app/Http/Controllers/ShopController.php
@@ -9,19 +9,27 @@ use App\Models\Shop;
 use App\Models\Area;
 use App\Models\Bookmark;
 use App\Models\Genre;
+use App\Models\Booking;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Redis;
 
 class ShopController extends Controller
 {
     public function index(Shop $shop) {
 
-        $user = Auth::user();
         $shops = Shop::with(['genre', 'area'])->get();
         $areas = Area::all();
         $genres = Genre::all();
 
-        // pluck：該当ユーザにブックマークされているお店のidを集めてくる
-        $bookmark = Bookmark::where('user_id', $user->id)
-        ->pluck('shop_id')->toArray();
+        $user = Auth::user();
+        $bookmark = [];
+
+        if(Auth::check()) {
+            // pluck：該当ユーザにブックマークされているお店のidを集めてくる
+            $bookmark = Bookmark::where('user_id', $user->id)
+            ->pluck('shop_id')->toArray();
+        }
 
         return view('index', compact('shops', 'areas', 'genres', 'bookmark'));
     }
@@ -75,6 +83,7 @@ class ShopController extends Controller
         $genres = Genre::all();
 
         $user = Auth::user();
+        $bookmark = [];
 
         if($user) {
             $bookmark = Bookmark::where('user_id', $user->id)
@@ -87,16 +96,71 @@ class ShopController extends Controller
         return view('index', compact('shops', 'areas', 'genres', 'selectedArea', 'selectedGenre', 'bookmark'));
     }
 
+    //お店の詳細ページ
+    public function detail($id)
+    {
+        $shop = Shop::with(['genre', 'area'])->find($id);
+
+        //予約可能期間
+        $today = Carbon::today()->toDateString();
+        $maxDate = Carbon::today()->addDays(90)->toDateString();
+        $minTime = Carbon::now()->addHours(1)->toTimeString();
+
+        return view('detail', compact('shop', 'today', 'maxDate'));
+    }
+
+    //予約処理
+    public function storeBooking(Request $request)
+    {
+        $user = Auth::user();
+
+        if($user) {
+            $booking = Booking::create([
+                'shop_id' => $request->shop_id,
+                'user_id'=> Auth::id(),
+                'date' => $request->date,
+                'time' => $request->time,
+                'number' => $request->number,
+            ]);
+            return redirect('done');
+        } else {
+            return redirect()->with('message', '予約するにはログインしてください');
+        }
+    }
+
+    public function destroyBooking(Request $request)
+    {
+        $booking = Booking::find($request->id);
+
+        if($booking){
+            $booking->delete();
+        }
+
+        return redirect('mypage');
+    }
+
     //マイページ
     public function viewMyPage()
     {
         $user = Auth::user();
+        $now = Carbon::now();
 
         $userBookmarks = Bookmark::where('user_id', $user->id)
         ->with('shop')
         ->get()
         ->pluck('shop');
 
-        return view('mypage', compact('userBookmarks'));
+        $userBookings = Booking::where('user_id', $user->id)
+        ->with('shop')
+        ->where('date', '>=', Carbon::today())
+        ->get();
+
+        return view('auth.mypage', compact('userBookmarks', 'userBookings'));
+    }
+
+    //予約完了ページ
+    public function viewDone()
+    {
+        return view('done');
     }
 }
